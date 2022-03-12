2d21929b77e27ee39e9a5b48e9074e46cc43d941
Pavan <pavanshirfule@gmail.com>
2022-03-12 22:12:06 +0530

    new project added

---
+++ InvestmentBanking/src/test/java/testcases/TC01.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.testng.Reporter;
+
+public class TC01 {
+
+	public void tc01login() {
+		Reporter.log("tc01 passed");
+	}
+	
+}
---
+++ InvestmentBanking/src/test/java/testcases/TC02.java
@@ -0,0 +1,10 @@
+package testcases;
+
+import org.testng.Reporter;
+
+public class TC02 {
+
+	public void tc02() {
+		Reporter.log("tc02 passed");
+	}
+}
